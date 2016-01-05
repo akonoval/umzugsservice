d1ef9844f2362f7d35ffc87234741d2f608c086c
Andriy Konoval <ko.andriy@gmail.com>
2016-01-06 00:33:53 +0100

    Initial commit

---
+++ js/umz_termine.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+        jQuery('.calendar #keinbus').parent().parent().parent().css("background-color", "#66ffb3");
+//        jQuery('#edit-field-datum-und-0-show-todate')[0].checked = false;
+//        jQuery('.form-item-field-datum-und-0-value2').hide();
+});
